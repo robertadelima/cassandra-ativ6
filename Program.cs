24553bd853817ba3d3194e36ed700e1fcffee6a2
robertadelima <robertarl@gmail.com>
2019-11-30 09:47:19 -0300

    added project solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cassandra_ativ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
